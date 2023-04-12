c06a4a08715fbe5a075aff877c0bccedfb25d3f0
zackjowett <zjwebdev@gmail.com>
2023-04-12 14:53:27 +1000

    possible splash design (likely won't pursue)

--- components/angledDiv/AngledDiv.tsx
+++ components/angledDiv/AngledDiv.tsx
@@ -4,25 +4,34 @@ export default function AngledDiv() {
 	return (
 		<div className={styles.wrapper}>
 			<div className={styles.rotateElement}>
-				<div className={styles.angledDiv}>
-					<div className={styles.text}>
-						<h1>Zachary Jowett</h1>
-					</div>
+				<div className={`${styles.text} ${styles.header}`}>
+					<h1>Zachary Jowett</h1>
 				</div>
-				<div className={styles.angledDiv}>
+				<div className={`${styles.overflowWrapper}`}>
 					<div className={styles.text}>
-						<h1>Projects</h1>
+						<h3>Projects</h3>
 					</div>
+					<div className={`${styles.angledDiv} ${styles.left}`}></div>
 				</div>
-				<div className={styles.angledDiv}>
+				<div className={`${styles.overflowWrapper} `}>
 					<div className={styles.text}>
-						<h1>Me</h1>
+						<h3>GitHub</h3>
 					</div>
+					<div
+						className={`${styles.angledDiv} ${styles.right}`}></div>
 				</div>
-				<div className={styles.angledDiv}>
+				<div className={`${styles.overflowWrapper} `}>
 					<div className={styles.text}>
-						<h1>Contact</h1>
+						<h3>Me</h3>
 					</div>
+					<div className={`${styles.angledDiv} ${styles.left}`}></div>
+				</div>
+				<div className={`${styles.overflowWrapper} `}>
+					<div className={styles.text}>
+						<h3>Contact</h3>
+					</div>
+					<div
+						className={`${styles.angledDiv} ${styles.right}`}></div>
 				</div>
 			</div>
 		</div>
