6c244626a7839fee54deae2044aea9c532357091
zackjowett <zjwebdev@gmail.com>
2023-04-12 14:57:45 +1000

    added key prop for element in map iterator

--- components/skill/skill.tsx
+++ components/skill/skill.tsx
@@ -12,8 +12,8 @@ export default function Skill({ title, subtitle = "", items }: Props) {
 			<h3>{title}</h3>
 			<h4>{subtitle}</h4>
 			<ul>
-				{items.map((item) => {
-					return <li>{item}</li>;
+				{items.map((item, index) => {
+					return <li key={index}>{item}</li>;
 				})}
 			</ul>
 		</div>
