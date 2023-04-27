04ed93e69ee2696802bab60af2bd285536561215
ZackJowett <zjwebdev@gmail.com>
2023-04-27 18:07:14 +1000

    improvements to responsiveness

--- components/nav/MainNav.tsx
+++ components/nav/MainNav.tsx
@@ -1,19 +1,30 @@
 import Button from "../objects/button/Button";
 import styles from "./MainNav.module.scss";
+import { FaQuestion } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
+import { BsGearWideConnected, BsQuestionLg } from "react-icons/bs";
+import { MdMailOutline } from "react-icons/md";
 
 export default function MainNav() {
+	const handleClick = () => {
+		console.log("clicked");
+	};
+
 	return (
 		<nav className={styles.nav}>
-			<div className={styles.link}>
+			<div className={styles.link} onClick={handleClick}>
 				<Button secondary>ABOUT</Button>
+				<BsQuestionLg className={styles.icon} />
 			</div>
 
-			<div className={styles.link}>
+			<div className={styles.link} onClick={handleClick}>
 				<Button primary>PROJECTS</Button>
+				<BsGearWideConnected className={styles.icon} />
 			</div>
 
-			<div className={styles.link}>
+			<div className={styles.link} onClick={handleClick}>
 				<Button secondary>CONTACT</Button>
+				<MdMailOutline className={styles.icon} />
 			</div>
 		</nav>
 	);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-import "@/styles/reset.scss";
 import "@/styles/globals.scss";
 
 import type { AppProps } from "next/app";
