146132a111b0728e3f3769ed5f3f30e62c4e2134
ZackJowett <zjwebdev@gmail.com>
2023-05-01 22:36:24 +1000

    added favicon and clickable color changing title

--- components/text/title/Title.tsx
+++ components/text/title/Title.tsx
@@ -1,16 +1,72 @@
-import MainNav from "@/components/nav/MainNav";
+import { useState } from "react";
 import styles from "./Title.module.scss";
-import Link from "next/link";
 
 interface Props {
 	className?: string;
 }
 
 export default function Title({ className }: Props) {
+	const [currColor, setCurrColor] = useState("#1f66ff");
+
+	function changeRootColor(color: string) {
+		setCurrColor(color);
+		document.documentElement.style.setProperty("--primary", color);
+	}
+
 	return (
-		<Link href="/" className={`${className} ${styles.title}`}>
-			<h1 className={styles.first}>ZACHARY</h1>
+		<div className={`${className} ${styles.title}`}>
+			<h1 className={styles.first}>
+				<span
+					className={`${styles.primary} ${
+						currColor == "#1f66ff" && styles.current
+					}`}
+					onClick={() => changeRootColor("#1f66ff")}>
+					Z
+				</span>
+				<span
+					className={`${styles.orange} ${
+						currColor == "orange" && styles.current
+					}`}
+					onClick={() => changeRootColor("orange")}>
+					A
+				</span>
+				<span
+					className={`${styles.grey} ${
+						currColor == "grey" && styles.current
+					}`}
+					onClick={() => changeRootColor("grey")}>
+					C
+				</span>
+				<span
+					className={`${styles.purple} ${
+						currColor == "purple" && styles.current
+					}`}
+					onClick={() => changeRootColor("purple")}>
+					H
+				</span>
+				<span
+					className={`${styles.red} ${
+						currColor == "red" && styles.current
+					}`}
+					onClick={() => changeRootColor("red")}>
+					A
+				</span>
+				<span
+					className={`${styles.green} ${
+						currColor == "green" && styles.current
+					}`}
+					onClick={() => changeRootColor("green")}>
+					R
+				</span>
+				<span
+					className={`${styles.lightBlue} ${
+						currColor == "lightblue" && styles.current
+					}`}
+					onClick={() => changeRootColor("lightblue")}>
+					Y
+				</span>
+			</h1>
 			<p className={styles.last}>JOWETT</p>
-		</Link>
+		</div>
 	);
 }
