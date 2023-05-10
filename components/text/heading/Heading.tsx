a40916ba1675128a8a493a749d2423b8e2f99774
ZackJowett <zjwebdev@gmail.com>
2023-05-10 11:13:07 +1000

    updated to new design

--- components/text/heading/Heading.tsx
+++ components/text/heading/Heading.tsx
@@ -3,13 +3,25 @@ import Link from "next/link";
 
 interface Props {
 	className?: string;
+	title: string;
+	subtitle?: string;
+	reverse?: boolean;
 }
 
-export default function Heading({ className }: Props) {
+export default function Heading({
+	className,
+	title,
+	subtitle,
+	reverse = false,
+}: Props) {
 	return (
-		<Link href="/" className={`${className} ${styles.title}`}>
-			<h1 className={styles.first}>Zack Jowett</h1>
-			<p className={styles.last}>Full-Stack Developer</p>
+		<Link
+			href="/"
+			className={`${className} ${styles.title} ${
+				reverse ? styles.reverse : ""
+			}`}>
+			<h1 className={styles.first}>{title}</h1>
+			<p className={styles.last}>{subtitle}</p>
 		</Link>
 	);
 }
