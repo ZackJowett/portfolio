676247ce7d6711b39991ac6020d46b740b3e4df0
ZackJowett <zjwebdev@gmail.com>
2023-05-10 11:13:56 +1000

    removed flex attributed (now located in layout)

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,58 +1,98 @@
 import Layout from "@/components/layouts/Layout";
-import Title from "@/components/text/title/Title";
+import BorderedSection from "@/components/sections/BorderedSection/BorderedSection";
+import Heading from "@/components/text/heading/Heading";
+import Navigator from "@/components/nav/Navigator";
+import { useEffect, useState } from "react";
+import Project from "@/components/project/project";
 import styles from "@/styles/pages/Projects.module.scss";
-import { useState } from "react";
-import { DiJqueryLogo } from "react-icons/di";
-import { FaShopify } from "react-icons/fa";
-import { SiAdobexd } from "react-icons/si";
 
 export default function Projects() {
-	const [page, setPage] = useState(null);
+	const [project, setProject] = useState("crr");
 
 	return (
-		<Layout className={styles.projects}>
-			<Title title="Projects" />
-			{/* <PageWrapper className={styles.content}>
-				<nav className={styles.pageNav}>
-					<Link href="/about">
-						<Button secondary>ABOUT</Button>
-					</Link>
-					<Link href="/projects">
-						<Button primary>PROJECTS</Button>
-					</Link>
-					<Link href="/contact">
-						<Button secondary>CONTACT</Button>
-					</Link>
-				</nav>
-				<nav className={styles.projectNav}>
-					<Link href="/about">
-						<Button secondary>ABOUT</Button>
-					</Link>
-					<Link href="/projects">
-						<Button primary>PROJECTS</Button>
-					</Link>
-					<Link href="/contact">
-						<Button secondary>CONTACT</Button>
-					</Link>
-				</nav>
-				<PageContent>
+		<Layout>
+			<BorderedSection>
+				<Heading title="Projects" subtitle="Zack Jowett" reverse />
+			</BorderedSection>
+			<Navigator currentProject={project} setProject={setProject} />
+			{project && (
+				<BorderedSection
+					className={`${styles.details} ${
+						project === "the-game"
+							? styles.center
+							: project === "crr"
+							? styles.left
+							: project === "fab"
+							? styles.right
+							: ""
+					}`}>
 					<Project
-						title="THE GAME"
+						show={project === "crr"}
+						title="Cricket's Rise Ranch"
+						role="Sole Developer & Technician"
+						tags={["Deployed"]}
+						year="2023"
+						client="Cricket's Rise Ranch"
+						desc="Designed and built with Next, integrated with Shopify, contains video streaming and live data from on-site sensors"
+						images={[]}
+					/>
+					<Project
+						show={project === "the-game"}
+						title="The Game"
 						role="Sole Developer"
-						projectDuration="6 Months"
-						projectStatus="Complete"
-						description="Redesigning of The Game Wallets' Shopify website."
-						technologies={[
-							{ name: "Shopify Liquid", icon: <FaShopify /> },
+						tags={["Complete"]}
+						year="2023"
+						client="The Game"
+						desc="Reimagined The Game Wallets’ existing website with a modern and mobile-friendly design, and created a custom monogramming app"
+						images={[
 							{
-								name: "jQuery",
-								icon: <DiJqueryLogo />,
+								src: "/static/images/the-game/the-game-home.png",
+								alt: "Home Page",
 							},
-							{ name: "Adobe XD", icon: <SiAdobexd /> },
 						]}
 					/>
-				</PageContent> */}
-			{/* </PageWrapper> */}
+					<Project
+						show={project === "fab"}
+						title="FAB Collection Manager"
+						role="Back-End & API Developer"
+						tags={["Complete"]}
+						year="2022"
+						client="University Capstone"
+						desc="Created a REST API and handled back-end integration"
+						images={[]}
+					/>
+				</BorderedSection>
+			)}
 		</Layout>
 	);
 }
+// function getSelectedProject(project: string) {
+// 	if (project === "the-game") {
+// 		return (
+// 			<Project
+// 				title="The Game"
+// 				role="Sole Developer"
+// 				status="Complete"
+// 				desc="Reimagined The Game Wallets’ existing website with a modern and mobile-friendly design"
+// 			/>
+// 		);
+// 	} else if (project === "crr") {
+// 		return (
+// 			<Project
+// 				title="Cricket's Rise Ranch"
+// 				role="Sole Developer"
+// 				status="Complete"
+// 				desc="..."
+// 			/>
+// 		);
+// 	} else if (project === "fab") {
+// 		return (
+// 			<Project
+// 				title="FAB Collection Manager"
+// 				role="Back-End & API Developer"
+// 				status="Complete"
+// 				desc="..."
+// 			/>
+// 		);
+// 	}
+// }
