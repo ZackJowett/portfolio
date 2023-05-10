06c701f9e670cd591961c99c4546a1ae08d8a593
zackjowett <zjwebdev@gmail.com>
2023-05-10 15:23:11 +1000

    added 'reverse' and 'small' options to component

--- components/text/title/Title.tsx
+++ components/text/title/Title.tsx
@@ -5,6 +5,8 @@ interface Props {
 	title: string;
 	subtitle?: string;
 	align?: "left" | "center" | "right";
+	small?: boolean;
+	reverse?: boolean;
 }
 
 export default function Title({
@@ -12,12 +14,21 @@ export default function Title({
 	title,
 	subtitle,
 	align = "center",
+	small,
+	reverse,
 }: Props) {
 	return (
 		<div
-			className={`${className} ${styles.wrapper}`}
+			className={`${className} ${styles.wrapper} ${
+				small ? styles.small : ""
+			} ${reverse ? styles.reverse : ""}`}
 			style={{ textAlign: align }}>
-			<h2 className={styles.title}>{title}</h2>
+			{small ? (
+				<h4 className={styles.title}>{title}</h4>
+			) : (
+				<h2 className={styles.title}>{title}</h2>
+			)}
+
 			{subtitle && <p className={styles.subtitle}>{subtitle}</p>}
 		</div>
 	);
