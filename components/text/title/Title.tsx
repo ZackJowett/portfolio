f2a8fcd8cb6ebb5c190fcc3787128e06e6194e5c
ZackJowett <zjwebdev@gmail.com>
2023-05-10 11:12:45 +1000

    added text align option to title

--- components/text/title/Title.tsx
+++ components/text/title/Title.tsx
@@ -4,11 +4,19 @@ interface Props {
 	className?: string;
 	title: string;
 	subtitle?: string;
+	align?: "left" | "center" | "right";
 }
 
-export default function Heading({ className, title, subtitle }: Props) {
+export default function Title({
+	className,
+	title,
+	subtitle,
+	align = "center",
+}: Props) {
 	return (
-		<div className={`${className} ${styles.wrapper}`}>
+		<div
+			className={`${className} ${styles.wrapper}`}
+			style={{ textAlign: align }}>
 			<h2 className={styles.title}>{title}</h2>
 			{subtitle && <p className={styles.subtitle}>{subtitle}</p>}
 		</div>
