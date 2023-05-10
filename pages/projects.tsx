87b95b53b6ca5ac747a14826d44b5ffca16c8bea
zackjowett <zjwebdev@gmail.com>
2023-05-10 15:22:20 +1000

    added project images

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -26,73 +26,76 @@ export default function Projects() {
 							? styles.right
 							: ""
 					}`}>
-					<Project
-						show={project === "crr"}
-						title="Cricket's Rise Ranch"
-						role="Sole Developer & Technician"
-						tags={["Deployed"]}
-						year="2023"
-						client="Cricket's Rise Ranch"
-						desc="Designed and built with Next, integrated with Shopify, contains video streaming and live data from on-site sensors"
-						images={[]}
-					/>
-					<Project
-						show={project === "the-game"}
-						title="The Game"
-						role="Sole Developer"
-						tags={["Complete"]}
-						year="2023"
-						client="The Game"
-						desc="Reimagined The Game Wallets’ existing website with a modern and mobile-friendly design, and created a custom monogramming app"
-						images={[
-							{
-								src: "/static/images/the-game/the-game-home.png",
-								alt: "Home Page",
-							},
-						]}
-					/>
-					<Project
-						show={project === "fab"}
-						title="FAB Collection Manager"
-						role="Back-End & API Developer"
-						tags={["Complete"]}
-						year="2022"
-						client="University Capstone"
-						desc="Created a REST API and handled back-end integration"
-						images={[]}
-					/>
+					{getSelectedProject(project)}
 				</BorderedSection>
 			)}
 		</Layout>
 	);
 }
-// function getSelectedProject(project: string) {
-// 	if (project === "the-game") {
-// 		return (
-// 			<Project
-// 				title="The Game"
-// 				role="Sole Developer"
-// 				status="Complete"
-// 				desc="Reimagined The Game Wallets’ existing website with a modern and mobile-friendly design"
-// 			/>
-// 		);
-// 	} else if (project === "crr") {
-// 		return (
-// 			<Project
-// 				title="Cricket's Rise Ranch"
-// 				role="Sole Developer"
-// 				status="Complete"
-// 				desc="..."
-// 			/>
-// 		);
-// 	} else if (project === "fab") {
-// 		return (
-// 			<Project
-// 				title="FAB Collection Manager"
-// 				role="Back-End & API Developer"
-// 				status="Complete"
-// 				desc="..."
-// 			/>
-// 		);
-// 	}
-// }
+function getSelectedProject(project: string) {
+	if (project === "the-game") {
+		return (
+			<Project
+				show={project === "the-game"}
+				title="The Game"
+				role="Sole Developer"
+				tags={["Launched", "2023"]}
+				client="The Game"
+				desc="Reimagined The Game Wallets’ existing website with a modern and mobile-friendly design, and created a custom monogramming app"
+				url="https://thegame.com/"
+				images={[
+					{
+						src: "/static/images/the-game/the-game-home.png",
+						alt: "Home Page",
+					},
+					{
+						src: "/static/images/the-game/the-game-product-list.png",
+						alt: "Product List",
+					},
+					{
+						src: "/static/images/the-game/the-game-product.png",
+						alt: "Product Details",
+					},
+					{
+						src: "/static/images/the-game/the-game-our-leather.png",
+						alt: "Our Leather",
+					},
+				]}
+			/>
+		);
+	} else if (project === "crr") {
+		return (
+			<Project
+				show={project === "crr"}
+				title="Cricket's Rise Ranch"
+				role="Sole Developer & Technician"
+				tags={["Launched", "2023"]}
+				client="Cricket's Rise Ranch"
+				desc="Designed and built with Next, integrated with Shopify, contains video streaming and live data from on-site sensors"
+				url="https://www.cricketsrise.com"
+				images={[
+					{
+						src: "/static/images/crr/crr-home.png",
+						alt: "Home Page",
+					},
+					{
+						src: "/static/images/crr/crr-live-data.png",
+						alt: "Live Data",
+					},
+				]}
+			/>
+		);
+	} else if (project === "fab") {
+		return (
+			<Project
+				show={project === "fab"}
+				title="FAB Collection Manager"
+				role="Back-End & API Developer"
+				tags={["University Capstone Project", "Complete", "2022"]}
+				client="University Capstone"
+				desc="Created a REST API and handled back-end integration"
+				images={[]}
+			/>
+		);
+	}
+}
