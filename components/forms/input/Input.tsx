d8220d5ec803499f0f12e918c8b02eeeb90008b3
ZackJowett <zjwebdev@gmail.com>
2023-05-11 15:22:55 +1000

    added value, onChange, and onBlur props

--- components/forms/input/Input.tsx
+++ components/forms/input/Input.tsx
@@ -5,6 +5,9 @@ interface Props {
 	name: string;
 	placeholder: string;
 	type: string;
+	value: any;
+	onChange: Function;
+	onBlur: Function;
 	icon?: React.ReactNode;
 }
 
@@ -13,17 +16,24 @@ export default function Input({
 	name,
 	placeholder,
 	type,
+	value,
+	onChange,
+	onBlur,
 	icon,
 }: Props) {
 	return (
 		<div className={styles.wrapper}>
 			{icon && <div className={styles.icon}>{icon}</div>}
 			<input
+				id={name}
 				name={name}
 				className={`${className} ${styles.input} ${
 					icon ? styles.leftAddon : ""
 				}`}
 				type={type}
+				value={value}
+				onChange={(e) => onChange(e)}
+				onBlur={(e) => onBlur(e)}
 				placeholder={placeholder}></input>
 		</div>
 	);
