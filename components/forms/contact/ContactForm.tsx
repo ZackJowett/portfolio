36c4c01f5c074e57918a6e04b139fc45dd089eca
zackjowett <zjwebdev@gmail.com>
2023-05-14 13:11:56 +1000

    added subject field

--- components/forms/contact/ContactForm.tsx
+++ components/forms/contact/ContactForm.tsx
@@ -7,6 +7,7 @@ import { FormikErrors, FormikValues, useFormik } from "formik";
 interface FormValues {
 	name: string;
 	email: string;
+	subject: string;
 	message: string;
 }
 
@@ -16,8 +17,8 @@ const validate = (values: FormValues) => {
 	const errors: FormikErrors<FormValues> = {};
 	if (!values.name) {
 		errors.name = "Required";
-	} else if (values.name.length > 15) {
-		errors.name = "Must be 15 characters or less";
+	} else if (values.name.length > 70) {
+		errors.name = "Must be 70 characters or less";
 	}
 
 	if (!values.email) {
@@ -26,10 +27,20 @@ const validate = (values: FormValues) => {
 		!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
 	) {
 		errors.email = "Invalid email address";
+	} else if (values.email.length > 320) {
+		errors.email = "Must be 320 characters or less";
+	}
+
+	if (!values.subject) {
+		errors.subject = "Required";
+	} else if (values.subject.length > 255) {
+		errors.subject = "Must be 255 characters or less";
 	}
 
 	if (!values.message) {
 		errors.message = "Required";
+	} else if (values.message.length > 10000) {
+		errors.message = "Must be 10,000 characters or less";
 	}
 
 	return errors;
@@ -41,10 +52,11 @@ export default function ContactForm() {
 		initialValues: {
 			name: "",
 			email: "",
+			subject: "",
 			message: "",
 		},
 		validate,
-		onSubmit: (values) => {
+		onSubmit: (values: FormValues) => {
 			// Called when form is submitted
 			alert(JSON.stringify(values, null, 2));
 		},
@@ -55,6 +67,11 @@ export default function ContactForm() {
 			<form className={styles.form} onSubmit={formik.handleSubmit}>
 				{/* ------ Name ------ */}
 				<Input
+					className={`${
+						formik.touched.name && formik.errors.name
+							? styles.error
+							: ""
+					}`}
 					type={"input"}
 					placeholder={"Name"}
 					// generates name, onChange, onBlur, value props
@@ -63,11 +80,16 @@ export default function ContactForm() {
 
 				{/* Error Message */}
 				{formik.touched.name && formik.errors.name ? (
-					<p>{formik.errors.name}</p>
+					<p className={styles.errorMessage}> {formik.errors.name}</p>
 				) : null}
 
 				{/* ------ Email ------*/}
 				<Input
+					className={`${
+						formik.touched.email && formik.errors.email
+							? styles.error
+							: ""
+					}`}
 					placeholder={"Email"}
 					type={"email"}
 					{...formik.getFieldProps("email")}
@@ -75,7 +97,28 @@ export default function ContactForm() {
 
 				{/* Error Message */}
 				{formik.touched.email && formik.errors.email ? (
-					<p>{formik.errors.email}</p>
+					<p className={styles.errorMessage}>{formik.errors.email}</p>
+				) : null}
+
+				{/* ------ Subj ------ */}
+				<Input
+					className={`${
+						formik.touched.subject && formik.errors.subject
+							? styles.error
+							: ""
+					}`}
+					type={"input"}
+					placeholder={"Subject"}
+					// generates name, onChange, onBlur, value props
+					{...formik.getFieldProps("subject")}
+				/>
+
+				{/* Error Message */}
+				{formik.touched.subject && formik.errors.subject ? (
+					<p className={styles.errorMessage}>
+						{" "}
+						{formik.errors.subject}
+					</p>
 				) : null}
 
 				{/* ------ Message ------ */}
@@ -86,18 +129,24 @@ export default function ContactForm() {
 					onChange={formik.handleChange}
 					onBlur={formik.handleBlur}
 					value={formik.values.message}
-					rows={10}
+					rows={500}
 					onInput={(e) => auto_grow(e.target)}
-					className={styles.textarea}
+					className={`${styles.textarea} ${
+						formik.touched.message && formik.errors.message
+							? styles.error
+							: ""
+					}`}
 				/>
 
 				{/* Error Message */}
 				{formik.touched.message && formik.errors.message ? (
-					<p>{formik.errors.message}</p>
+					<p className={styles.errorMessage}>
+						{formik.errors.message}
+					</p>
 				) : null}
 
 				{/* ------ Submit ------ */}
-				<Button title="Send" submit />
+				<Button submit title="Send" className={styles.submit} />
 			</form>
 		</div>
 	);
